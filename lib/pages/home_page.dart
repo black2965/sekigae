933eac36d2dc7c727bc349d2e7f2c1c50073236a
yum <chroma31415@gmail.com>
2023-03-07 17:15:17 +0900

    Asyncによるcontextのエラーを修正

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:sekigae/pages/customize_page.dart';
 
-
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -18,46 +17,42 @@ class _HomePageState extends State<HomePage> {
         title: const Center(child: Text("席替え侍")),
       ),
       body: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Text(
-            "席替え侍",
-            style: TextStyle(fontSize: 50),
-          ),
-          Text(
-            "開発版",
-            style: TextStyle(fontSize: 25),
-          ),
-          SizedBox(height: 50),
-          ElevatedButton(
-            child: Text("CSVファイルを選ぶ"),
-            onPressed: () async{
-              FilePickerResult? result = await FilePicker.platform.pickFiles(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        const Text(
+          "席替え侍",
+          style: TextStyle(fontSize: 50),
+        ),
+        const Text(
+          "開発版",
+          style: TextStyle(fontSize: 25),
+        ),
+        const SizedBox(height: 50),
+        ElevatedButton(
+          child: const Text("CSVファイルを選ぶ"),
+          onPressed: () async {
+            FilePickerResult? result = await FilePicker.platform.pickFiles(
                 type: FileType.custom,
                 allowedExtensions: ['csv'],
-                dialogTitle: "CSVファイルを選んでください"
-              );
-              if (result != null) {
-                print(result.paths);
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => CustomizePage()));
-              } else {
-
-              }
-            },
-          ),
-          SizedBox(height: 50),
-          TextButton(
-            onPressed: () {
-              showAboutDialog(
+                dialogTitle: "CSVファイルを選んでください");
+            if (result != null) {
+              print(result.paths);
+              if (!mounted) return;
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => CustomizePage()));
+            } else {}
+          },
+        ),
+        const SizedBox(height: 50),
+        TextButton(
+          onPressed: () {
+            showAboutDialog(
                 context: context,
                 applicationName: "席替え侍",
-                applicationVersion: "開発版"
-              );
-            },
-            child: Text("席替え侍について"),
-          )
-        ]
-      )),
+                applicationVersion: "開発版");
+          },
+          child: const Text("席替え侍について"),
+        )
+      ])),
     );
   }
 }
