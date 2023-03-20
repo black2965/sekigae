78b28dbfc91772d3fa8895056c50fe5e317c5ae8
小山田 純 - Oyamada Jun <oyamadajun@outlook.jp>
2023-03-20 13:43:59 +0900

    ホームへ戻るボタンを追加

--- lib/pages/result_page.dart
+++ lib/pages/result_page.dart
@@ -13,6 +13,29 @@ class _ResultPageState extends State<ResultPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("席替え結果"),
+        actions: <Widget>[
+          ElevatedButton.icon(
+            onPressed: () => showDialog<String>(
+              context: context,
+              builder: (BuildContext context) => AlertDialog(
+                title: const Text('ホームへ戻りますか？'),
+                content: const Text('席替え結果は破棄され、二度と見ることができなくなります。'),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text('キャンセル'),
+                  ),
+                  TextButton(
+                    onPressed: () => Navigator.popUntil(context, (route) => route.isFirst),
+                    child: const Text('ホームへ戻る'),
+                  ),
+                ],
+              ),
+            ),
+            icon: const Icon(Icons.home),
+            label: const Text("ホームへ戻る")
+          )
+        ]
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {  },
