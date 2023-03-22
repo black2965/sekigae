bfb13e24156f8d2884a06dca32ef0fc6d83ee581
yum <chroma31415@gmail.com>
2023-03-22 19:38:21 +0900

    席表のWidgetを作成

--- lib/ui/seat_table.dart
+++ lib/ui/seat_table.dart
@@ -1,12 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:sekigae/util/seat.dart';
 
 class SeatTable extends StatelessWidget {
-  final width;
-  final height;
+  final Seat seat;
+  final int height;
+  final int width;
+  late final List<Map<String, dynamic>> list;
+  late final int horizontal; //縦の行数
+  late final int vertical; //横の列数
+  late final bool isAlignLeft; //左よりかどうか
+  late final Table table;
 
-  SeatTable({Key? key, required this.height, required this.width, required Map<String>})
-      : super(key: key);
+  late final double boxHeight; //マス目の高さ
+  late final double boxWidth; //マス目の幅
+
+  SeatTable(
+      {Key? key, required this.height, required this.width, required this.seat})
+      : super(key: key) {
+    //*必要な条件を代入していく
+    list = seat.resultSeats;
+    vertical = seat.column;
+    horizontal = (list.length / vertical).ceil();
+    isAlignLeft = seat.isAlignLeft;
+
+    //1マスの高さ 幅を計算
+    boxHeight = height / horizontal;
+    boxWidth = width / vertical;
+
+    //*余る席の処理
+    //余りの人数分 空の人間を作成
+    for (int i = 0; i < horizontal * vertical - list.length; i++) {
+      Map<String, dynamic> map = {
+        "number": null,
+        "name": " ",
+        "front": false,
+      };
+      list.add(map);
+    }
+
+    //*Tableを作成
+    //Tableのchildrenに渡す配列
+    List<TableRow> assembly = [];
+    //行数ぶんTableRowを作成
+    for (int i = 0; i < horizontal; i++) {
+      //TableRowのchildrenに渡す配列
+      List<TableCell> cell = [];
+      //列数ぶんTableCellを作成
+      for (int j = 0; j < vertical; j++) {
+        //人の情報を取得
+        Map<String, dynamic> person = list[i * horizontal + j];
+        int? numInt = person["number"];
+        //number != null ならそのまま表示
+        String numStr = numInt != null ? numInt.toString() : " ";
+        String name = person["name"];
+        //マスの中身を作成
+        SizedBox content = SizedBox(
+          height: boxHeight,
+          width: boxWidth,
+          child: Center(child: Text("$numStr $name")),
+        );
+        TableCell box = TableCell(child: content);
+
+        cell.add(box);
+
+        //最後の行なら寄せの判定
+        if (i == horizontal - 1) {
+          //trueなら処理なしで既に左に寄っている
+          if (isAlignLeft == false) {
+            cell = List.from(cell.reversed);
+          }
+        }
+      }
+      TableRow row = TableRow(children: cell);
+      assembly.add(row);
+    }
+    //ListをもとにTableを作成
+    table = Table(
+      children: assembly,
+      border: TableBorder.all(color: Colors.black, width: 1),
+    );
+  }
 
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.black, width: 1),
+        ),
+        height: height.toDouble() + 2,
+        width: width.toDouble() + 2,
+        child: table,
+      ),
+    );
+  }
 }
