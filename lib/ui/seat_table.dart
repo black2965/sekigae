451c7cfe8f6a5fb80ff0678cdc836b936215fd6f
yum <chroma31415@gmail.com>
2023-03-22 16:00:51 +0900

    seat_tableを作成

---
+++ lib/ui/seat_table.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SeatTable extends StatelessWidget {
+  final width;
+  final height;
+
+  SeatTable({Key? key, required this.height, required this.width, required Map<String>})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {}
+}
