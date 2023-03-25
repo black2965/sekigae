f41fa8775d2293d425956c77509072699696611c
yum <chroma31415@gmail.com>
2023-03-26 01:10:29 +0900

    レイアウトの幅を調整

--- lib/pages/guide_page.dart
+++ lib/pages/guide_page.dart
@@ -10,97 +10,121 @@ class GuidePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppTheme.background_color,
       appBar: AppBar(
-          backgroundColor:AppTheme.app_bar_color ,
-          title: const Text('名簿ファイルの作り方')
-      ),
+          backgroundColor: AppTheme.app_bar_color,
+          title: const Text('名簿ファイルの作り方')),
       body: Center(
-        child: SizedBox(
-          width: 700,
-          child: ListView(
-            children: [
-              Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Text('1. 表計算ソフトを開く',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      SizedBox(height: 5,),
-                      Text('Microsoft Excel、LibreOffice Calc などの表計算ソフトを開いてください。')
-                    ],
-                  ),
-                ),
-              ),
-              Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const[
-                      Text('2. 名簿を作る',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      SizedBox(height: 5,),
-                      Text('次の形式で名簿を作成してください'),
-                      Text('  A列: 出席番号'),
-                      Text('  B列: 名前'),
-                      Text('  C列: 前方の席を希望するかどうか'),
-                      Text('  　　　希望する場合→ 1'),
-                      Text('  　　　希望しない場合→ 0 または何も入力しない'),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Image(image: AssetImage('assets/images/guide_page_example.png'),height: 150,
+        child: Container(
+          padding: EdgeInsets.all(30),
+          child: Container(
+            width: 700,
+            child: ListView(
+              children: [
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          '1. 表計算ソフトを開く',
+                          style: TextStyle(fontSize: 20),
                         ),
-                      )
-                    ],
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                            'Microsoft Excel、LibreOffice Calc などの表計算ソフトを開いてください。')
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const[
-                      Text('3. CSVファイルで出力する',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      SizedBox(height: 5,),
-                      Text('Microsoft Excel',
-                        style: TextStyle(fontSize: 18),
-                      ),
-                      Text('  ファイル ＞ 名前を付けて保存 と進み、ファイル名の下にあるドロップダウンリストで『CSV (コンマ区切り) (*.csv)』を選んで、『保存』をクリックしてください。'),
-                      SizedBox(height: 5,),
-                      Text('LibreOffice Calc',
-                        style: TextStyle(fontSize: 18),
-                      ),
-                      Text('  ファイル(F) ＞ 名前を付けて保存(A)... と進み、開いたダイアログの右下のドロップダウンリストで『テキスト CSV(.csv)』を選んで、『保存(S)』をクリックしてください。'),
-                    ],
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          '2. 名簿を作る',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text('次の形式で名簿を作成してください'),
+                        Text('  A列: 出席番号'),
+                        Text('  B列: 名前'),
+                        Text('  C列: 前方の席を希望するかどうか'),
+                        Text('  　　　希望する場合→ 1'),
+                        Text('  　　　希望しない場合→ 0 または何も入力しない'),
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Image(
+                            image: AssetImage(
+                                'assets/images/guide_page_example.png'),
+                            height: 150,
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const[
-                      Text('4. このアプリで読み込む',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      SizedBox(height: 5,),
-                      Text('　このアプリのトップ画面の『名簿ファイルを読み込む(.csv)』をクリックして、表計算ソフトで出力したCSVファイルを選択してください。')
-                    ],
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          '3. CSVファイルで出力する',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          'Microsoft Excel',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        Text(
+                            '  ファイル ＞ 名前を付けて保存 と進み、ファイル名の下にあるドロップダウンリストで『CSV (コンマ区切り) (*.csv)』を選んで、『保存』をクリックしてください。'),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          'LibreOffice Calc',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        Text(
+                            '  ファイル(F) ＞ 名前を付けて保存(A)... と進み、開いたダイアログの右下のドロップダウンリストで『テキスト CSV(.csv)』を選んで、『保存(S)』をクリックしてください。'),
+                      ],
+                    ),
                   ),
                 ),
-              )
-            ],
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          '4. このアプリで読み込む',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                            '　このアプリのトップ画面の『名簿ファイルを読み込む(.csv)』をクリックして、表計算ソフトで出力したCSVファイルを選択してください。')
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
-
--- lib/pages/info_page.dart
+++ lib/pages/info_page.dart
@@ -22,89 +22,94 @@ class _InfoPageState extends State<InfoPage> {
         ),
         body: Center(
           child: Container(
-            width: 700,
-            child: ListView(
-              children: [
-                const Card(
-                  child: ListTile(
-                    leading: Icon(Icons.apps),
-                    title: Text('アプリ名'),
-                    subtitle: Text('席替え侍'),
+            padding: EdgeInsets.all(30),
+            child: Container(
+              width: 700,
+              child: ListView(
+                children: [
+                  const Card(
+                    child: ListTile(
+                      leading: Icon(Icons.apps),
+                      title: Text('アプリ名'),
+                      subtitle: Text('席替え侍'),
+                    ),
                   ),
-                ),
-                const Card(
-                  child: ListTile(
-                    leading: Icon(Icons.verified),
-                    title: Text('バージョン'),
-                    subtitle: Text('開発版'),
+                  const Card(
+                    child: ListTile(
+                      leading: Icon(Icons.verified),
+                      title: Text('バージョン'),
+                      subtitle: Text('開発版'),
+                    ),
                   ),
-                ),
-                Card(
-                  child: ListTile(
-                      onTap: () async {
-                        if (await canLaunchUrlString(
-                            "https://www.gnu.org/licenses/gpl-3.0.html")) {
-                          await launchUrlString(
-                              "https://www.gnu.org/licenses/gpl-3.0.html");
-                        }
-                      },
-                      leading: const Icon(Icons.book),
-                      title: const Text('このアプリのライセンス'),
-                      subtitle:
-                          const Text('GNU General Public License v3.0 or later'),
-                      trailing: const Icon(Icons.navigate_next)),
-                ),
-                const SizedBox(height: 20),
-                const Card(
-                  child: ListTile(
-                    leading: Icon(Icons.person),
-                    title: Text('開発者集団『みかんさん』'),
-                    subtitle: Text('Xkuroiwa noriox 小山田純'),
+                  Card(
+                    child: ListTile(
+                        onTap: () async {
+                          if (await canLaunchUrlString(
+                              "https://www.gnu.org/licenses/gpl-3.0.html")) {
+                            await launchUrlString(
+                                "https://www.gnu.org/licenses/gpl-3.0.html");
+                          }
+                        },
+                        leading: const Icon(Icons.book),
+                        title: const Text('このアプリのライセンス'),
+                        subtitle:
+                            const Text('GNU General Public License v3.0 or later'),
+                        trailing: const Icon(Icons.navigate_next)),
                   ),
-                ),
-                Card(
-                  child: ListTile(
-                      onTap: () async {
-                        if (await canLaunchUrlString("https://mican3.github.io/sekigae.html")) {
-                          await launchUrlString("https://mican3.github.io/sekigae.html");
-                        }
-                      },
-                      leading: const Icon(Icons.home),
-                      title: const Text('ホームページ'),
-                      subtitle: const Text('https://mican3.github.io/sekigae.html'),
-                      trailing: const Icon(Icons.open_in_new)),
-                ),
-                Card(
-                  child: ListTile(
-                      onTap: () async{
-                        if (await canLaunchUrlString("https://github.com/sis314/sekigae")) {
-                        await launchUrlString("https://github.com/sis314/sekigae");
-                        }
-                      },
-                      leading: const Icon(Icons.code),
-                      title: const Text('ソースコード(Github)'),
-                      subtitle: const Text('https://github.com/sis314/sekigae'),
-                      trailing: const Icon(Icons.open_in_new)),
-                ),
-                const SizedBox(height: 20),
-                Card(
-                  child: ListTile(
-                      onTap: () {
-                        showLicensePage(
-                          context: context,
-                          applicationName: '席替え侍',
-                          applicationVersion: '開発版',
-                          applicationIcon: const FlutterLogo(),
-                          applicationLegalese:
-                              'GNU General Public License v3.0 or later',
-                        );
-                      },
-                      leading: const Icon(Icons.code),
-                      title: const Text('使用したライブラリー'),
-                      subtitle: const Text(''),
-                      trailing: const Icon(Icons.navigate_next)),
-                ),
-              ],
+                  const SizedBox(height: 20),
+                  const Card(
+                    child: ListTile(
+                      leading: Icon(Icons.person),
+                      title: Text('開発者集団『みかんさん』'),
+                      subtitle: Text('Xkuroiwa noriox 小山田純'),
+                    ),
+                  ),
+                  Card(
+                    child: ListTile(
+                        onTap: () async {
+                          if (await canLaunchUrlString("https://mican3.github.io/sekigae.html")) {
+                            await launchUrlString("https://mican3.github.io/sekigae.html");
+                          }
+                        },
+                        leading: const Icon(Icons.home),
+                        title: const Text('ホームページ'),
+                        subtitle: const Text('https://mican3.github.io/sekigae.html'),
+                        trailing: const Icon(Icons.open_in_new)),
+                  ),
+                  Card(
+                    child: ListTile(
+                        onTap: () async{
+                          if (await canLaunchUrlString("https://github.com/sis314/sekigae")) {
+                          await launchUrlString("https://github.com/sis314/sekigae");
+                          }
+                        },
+                        leading: const Icon(Icons.code),
+                        title: const Text('ソースコード(Github)'),
+                        subtitle: const Text('https://github.com/sis314/sekigae'),
+                        trailing: const Icon(Icons.open_in_new)),
+                  ),
+                  const SizedBox(height: 20),
+                  Card(
+                    child: ListTile(
+                        onTap: () {
+                          showLicensePage(
+                            context: context,
+                            applicationName: '席替え侍',
+                            applicationVersion: '1.0',
+                            applicationIcon: const ImageIcon(
+                              AssetImage('/assets/icon/icon.png'),
+                            ),
+                            applicationLegalese:
+                                'GNU General Public License v3.0 or later',
+                          );
+                        },
+                        leading: const Icon(Icons.code),
+                        title: const Text('使用したライブラリー'),
+                        subtitle: const Text(''),
+                        trailing: const Icon(Icons.navigate_next)),
+                  ),
+                ],
+              ),
             ),
           ),
         ));
